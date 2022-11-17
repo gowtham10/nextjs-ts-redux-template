fca166a7122f06b95a4a7628c6ef0fa2af875511
Gowtham Subramani <subramani.gowtham@gmail.com>
2022-11-17 11:28:07 +0530

    initial commit

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import styles from '@/components/button/button.module.css';
 import ButtonProps from '@/components/button/button.interface';
 
-const Button = (props: ButtonProps) => {
+function Button(props: ButtonProps) {
   const { buttonName, clickHandler } = props;
 
   return (
-    <button className={`${styles.button} ${styles.buttonPrimary}`} onClick={clickHandler}>
+    <button type='button' className={`${styles.button} ${styles.buttonPrimary}`} onClick={clickHandler}>
       {buttonName}
     </button>
   );
-};
+}
 
 export default Button;
--- worker/index.ts
+++ worker/index.ts
@@ -1,146 +1,146 @@
-export {};
-// import { skipWaiting, clientsClaim } from 'workbox-core';
-// import { ExpirationPlugin } from 'workbox-expiration';
-// import { NetworkFirst, CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
-// import { registerRoute, setDefaultHandler } from 'workbox-routing';
-// import { cleanupOutdatedCaches } from 'workbox-precaching';
+/* eslint import/no-extraneous-dependencies: "off" */
+import { skipWaiting, clientsClaim } from 'workbox-core';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { NetworkFirst, CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { registerRoute, setDefaultHandler } from 'workbox-routing';
+import { cleanupOutdatedCaches } from 'workbox-precaching';
 
-// skipWaiting();
-// clientsClaim();
+skipWaiting();
+clientsClaim();
 
-// // TODO: Ref https://medium.com/proximity-labs/building-a-next-js-pwa-using-next-pwa-and-service-worker-a7acb0ea54bc
+// TODO: Ref https://medium.com/proximity-labs/building-a-next-js-pwa-using-next-pwa-and-service-worker-a7acb0ea54bc
 
-// // must include following lines when using inject manifest module from workbox
-// // https://developers.google.com/web/tools/workbox/guides/precache-files/workbox-build#add_an_injection_point
+// must include following lines when using inject manifest module from workbox
+// https://developers.google.com/web/tools/workbox/guides/precache-files/workbox-build#add_an_injection_point
 
-// // const WB_MANIFEST = self.__WB_MANIFEST;
-// // // Precache fallback route and image
-// // WB_MANIFEST.push({
-// //   url: '/fallback',
-// //   revision: '1234567890',
-// // });
-// // precacheAndRoute(WB_MANIFEST);
+// const WB_MANIFEST = self.__WB_MANIFEST;
+// // Precache fallback route and image
+// WB_MANIFEST.push({
+//   url: '/fallback',
+//   revision: '1234567890',
+// });
+// precacheAndRoute(WB_MANIFEST);
 
-// cleanupOutdatedCaches();
-// registerRoute(
-//   '/',
-//   new NetworkFirst({
-//     cacheName: 'start-url',
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 1,
-//         maxAgeSeconds: 86400,
-//         purgeOnQuotaError: !0,
-//       }),
-//     ],
-//   }),
-//   'GET'
-// );
-// registerRoute(
-//   /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-//   new CacheFirst({
-//     cacheName: 'google-fonts',
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 4,
-//         maxAgeSeconds: 31536e3,
-//         purgeOnQuotaError: !0,
-//       }),
-//     ],
-//   }),
-//   'GET'
-// );
-// registerRoute(
-//   /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-//   new StaleWhileRevalidate({
-//     cacheName: 'static-font-assets',
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 4,
-//         maxAgeSeconds: 604800,
-//         purgeOnQuotaError: !0,
-//       }),
-//     ],
-//   }),
-//   'GET'
-// );
-// // disable image cache, so we could observe the placeholder image when offline
+cleanupOutdatedCaches();
+registerRoute(
+  '/',
+  new NetworkFirst({
+    cacheName: 'start-url',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 1,
+        maxAgeSeconds: 86400,
+        purgeOnQuotaError: !0,
+      }),
+    ],
+  }),
+  'GET'
+);
+registerRoute(
+  /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+  new CacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 4,
+        maxAgeSeconds: 31536e3,
+        purgeOnQuotaError: !0,
+      }),
+    ],
+  }),
+  'GET'
+);
+registerRoute(
+  /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+  new StaleWhileRevalidate({
+    cacheName: 'static-font-assets',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 4,
+        maxAgeSeconds: 604800,
+        purgeOnQuotaError: !0,
+      }),
+    ],
+  }),
+  'GET'
+);
+// disable image cache, so we could observe the placeholder image when offline
 
-// registerRoute(
-//   /\.(?:js)$/i,
-//   new StaleWhileRevalidate({
-//     cacheName: 'static-js-assets',
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 32,
-//         maxAgeSeconds: 86400,
-//         purgeOnQuotaError: !0,
-//       }),
-//     ],
-//   }),
-//   'GET'
-// );
-// registerRoute(
-//   /\.(?:css|less)$/i,
-//   new StaleWhileRevalidate({
-//     cacheName: 'static-style-assets',
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 32,
-//         maxAgeSeconds: 86400,
-//         purgeOnQuotaError: !0,
-//       }),
-//     ],
-//   }),
-//   'GET'
-// );
-// registerRoute(
-//   /\.(?:json|xml|csv)$/i,
-//   new NetworkFirst({
-//     cacheName: 'static-data-assets',
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 32,
-//         maxAgeSeconds: 86400,
-//         purgeOnQuotaError: !0,
-//       }),
-//     ],
-//   }),
-//   'GET'
-// );
-// registerRoute(
-//   /\/api\/.*$/i,
-//   new NetworkFirst({
-//     cacheName: 'apis',
-//     networkTimeoutSeconds: 10,
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 16,
-//         maxAgeSeconds: 86400,
-//         purgeOnQuotaError: !0,
-//       }),
-//     ],
-//   }),
-//   'GET'
-// );
-// registerRoute(
-//   /.*/i,
-//   new NetworkFirst({
-//     cacheName: 'others',
-//     networkTimeoutSeconds: 10,
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 32,
-//         maxAgeSeconds: 86400,
-//         purgeOnQuotaError: !0,
-//       }),
-//     ],
-//   }),
-//   'GET'
-// );
+registerRoute(
+  /\.(?:js)$/i,
+  new StaleWhileRevalidate({
+    cacheName: 'static-js-assets',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 32,
+        maxAgeSeconds: 86400,
+        purgeOnQuotaError: !0,
+      }),
+    ],
+  }),
+  'GET'
+);
+registerRoute(
+  /\.(?:css|less)$/i,
+  new StaleWhileRevalidate({
+    cacheName: 'static-style-assets',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 32,
+        maxAgeSeconds: 86400,
+        purgeOnQuotaError: !0,
+      }),
+    ],
+  }),
+  'GET'
+);
+registerRoute(
+  /\.(?:json|xml|csv)$/i,
+  new NetworkFirst({
+    cacheName: 'static-data-assets',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 32,
+        maxAgeSeconds: 86400,
+        purgeOnQuotaError: !0,
+      }),
+    ],
+  }),
+  'GET'
+);
+registerRoute(
+  /\/api\/.*$/i,
+  new NetworkFirst({
+    cacheName: 'apis',
+    networkTimeoutSeconds: 10,
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 16,
+        maxAgeSeconds: 86400,
+        purgeOnQuotaError: !0,
+      }),
+    ],
+  }),
+  'GET'
+);
+registerRoute(
+  /.*/i,
+  new NetworkFirst({
+    cacheName: 'others',
+    networkTimeoutSeconds: 10,
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 32,
+        maxAgeSeconds: 86400,
+        purgeOnQuotaError: !0,
+      }),
+    ],
+  }),
+  'GET'
+);
 
-// // following lines gives you control of the offline fallback strategies
-// // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#comprehensive_fallbacks
+// following lines gives you control of the offline fallback strategies
+// https://developers.google.com/web/tools/workbox/guides/advanced-recipes#comprehensive_fallbacks
 
-// // Use a stale-while-revalidate strategy for all other requests.
-// setDefaultHandler(new StaleWhileRevalidate());
+// Use a stale-while-revalidate strategy for all other requests.
+setDefaultHandler(new StaleWhileRevalidate());
